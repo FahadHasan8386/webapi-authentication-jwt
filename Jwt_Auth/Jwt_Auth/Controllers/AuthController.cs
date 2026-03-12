93e640fcbacc22b432b2b1fa0051ba15c0b5f292
Fahad Hasan <hasan.fahad67@outlook.com>
2026-03-12 14:37:22 +0600

    Static Password Hashing APi

---
+++ Jwt_Auth/Jwt_Auth/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Jwt_Auth.Entities;
+using Jwt_Auth.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt_Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        public static User user = new();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto userDto)
+        {
+            var hasedPassword = new PasswordHasher<User>().HashPassword(user , userDto.Password);
+
+            user.Username = userDto.UserName;
+            user.PasswordHash = hasedPassword;
+
+            return Ok(user);
+        }
+    }
+}
---
+++ Jwt_Auth/Jwt_Auth/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Jwt_Auth.Models
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
