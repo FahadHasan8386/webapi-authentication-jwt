87b63434d0d2c6d7d11a8ea6f83c92d79be7ce59
Fahad Hasan <hasan.fahad67@outlook.com>
2026-05-26 22:04:25 +0600

    Fix project

--- Jwt_Auth/Jwt_Auth/Program.cs
+++ Jwt_Auth/Jwt_Auth/Program.cs
@@ -1,19 +1,43 @@
 using Jwt_Auth.Data;
+using Jwt_Auth.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container 
+// Controllers
 builder.Services.AddControllers();
 
-// Swagger/OpenAPI Setup 
+// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//  Database Connection Setup
+// Database
 builder.Services.AddDbContext<UserDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// Dependency Injection
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+// JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true
+        };
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -25,6 +49,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Authentication আগে
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
