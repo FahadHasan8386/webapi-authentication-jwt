275c0545a3fdc646d61b4fbcaf1632ef320ef3e6
Fahad Hasan <hasan.fahad67@outlook.com>
2026-07-04 14:45:16 +0600

    Fix jwt Controller

--- Jwt_Auth/Jwt_Auth/Controllers/AuthController.cs
+++ Jwt_Auth/Jwt_Auth/Controllers/AuthController.cs
@@ -36,6 +36,17 @@ namespace Jwt_Auth.Controllers
 
             return Ok(result);
         }
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<TokenResponseDto>> RefreshToken(RefreshTokenRequestDto request)
+        {
+            var result = await authService.RefreshTokenAsync(request);
+            if (result is null || result.AccessToken is null || result.RefreshToken is null)
+                return Unauthorized("Invalid refresh token.");
+
+            return Ok(result);
+        }
+
         [Authorize]
         [HttpGet]
         public IActionResult AuthenticatedOnlyEndpoint()
